a1a83da187d3edb88f2e11020b9f59088a2ee4cd
gids-dadi <gideons564@gmail.com>
2022-04-25 22:04:07 +0100

    Fix errors

--- modules/AwesomeBooks.js
+++ modules/AwesomeBooks.js
@@ -1,32 +1,32 @@
 import Book from './book.js';
 
 class AwesomeBooks {
-  constructor () {
+  constructor() {
     this.books = [];
   }
 
- forBook(title,author) {
-  const id = this.books.length + 1;
-   const book = new Book(title, author, id);
-   this.books.push(book);
-   this.#save();
-}
+  forBook(title, author) {
+    const id = this.books.length + 1;
+    const book = new Book(title, author, id);
+    this.books.push(book);
+    this.#save();
+  }
 
- #ForBooks(books) {
+  #ForBooks(books) {
     books.forEach((book) => {
-    this.forBook(book.title, book.author);
+      this.forBook(book.title, book.author);
     });
   }
 
   getBooks() {
-  return this.books;
+    return this.books;
   }
 
-#save() {
-  localStorage.setItem('books', JSON.stringify(this.books));
+  #save() {
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
-    load() {
+  load() {
     const books = JSON.parse(localStorage.getItem('books'));
     if (books) {
       this.#ForBooks(books);
--- modules/index.js
+++ modules/index.js
@@ -4,13 +4,13 @@ import { DateTime } from '../luxon.js';
 const awesomeBooks = new AwesomeBooks();
 awesomeBooks.load();
 if (awesomeBooks.getBooks().length < 1) {
-    awesomeBooks.forBook(
-      'The Decline and Fall of the Roman Empire',
-      'Edward Gibbon	'
-    );
-    awesomeBooks.forBook('Far from the Madding Crowd', 'Thomas Hardy');
-    awesomeBooks.forBook('	Alice in Wonderland', 'Lewis Carrol');
-    awesomeBooks.forBook('A Dangerous place', 'D.P. Moynihan');
+  awesomeBooks.forBook(
+    'The Decline and Fall of the Roman Empire',
+    'Edward Gibbon	'
+  );
+  awesomeBooks.forBook('Far from the Madding Crowd', 'Thomas Hardy');
+  awesomeBooks.forBook('	Alice in Wonderland', 'Lewis Carrol');
+  awesomeBooks.forBook('A Dangerous place', 'D.P. Moynihan');
 }
 
 let bookList = document.querySelector('#bookList');
@@ -34,19 +34,19 @@ renderBooklist();
 
 const addBookForm = document.querySelector('#bookEntry');
 addBookForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const title = event.target.querySelector('#bookTitle').value;
-    const author = event.target.querySelector('#bookAuthor').value;
-    awesomeBooks.forBook(title, author);
-    this.reset();
-    renderBooklist();
+  event.preventDefault();
+  const title = event.target.querySelector('#bookTitle').value;
+  const author = event.target.querySelector('#bookAuthor').value;
+  awesomeBooks.forBook(title, author);
+  this.reset();
+  renderBooklist();
 });
 
 bookList.addEventListener('click', (event) => {
   if (event.target.classList.contains('remove')) {
-      const { id } = event.target.dataset;
-      awesomeBooks.deleteBook(+id);
-      renderBooklist();
+    const { id } = event.target.dataset;
+    awesomeBooks.deleteBook(+id);
+    renderBooklist();
   }
 });
 
@@ -59,30 +59,30 @@ const contactSection = document.querySelector('#contact-section');
 const newSection = document.querySelector('#new-book');
 
 list_link.addEventListener('click', () => {
-    list_link.classList.add('active');
-    new_link.classList.remove('active');
-    contact_link.classList.remove('active');
-    listSection.classList.remove('hidden');
-    contactSection.classList.add('hidden');
-    newSection.classList.add('hidden');
+  list_link.classList.add('active');
+  new_link.classList.remove('active');
+  contact_link.classList.remove('active');
+  listSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+  newSection.classList.add('hidden');
 });
 
 new_link.addEventListener('click', () => {
-    list_link.classList.remove('active');
-    new_link.classList.add('active');
-    contact_link.classList.remove('active');
-    listSection.classList.remove('hidden');
-    contactSection.classList.add('hidden');
-    newSection.classList.remove('hidden');
+  list_link.classList.remove('active');
+  new_link.classList.add('active');
+  contact_link.classList.remove('active');
+  listSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+  newSection.classList.remove('hidden');
 });
 
 contact_link.addEventListener('click', () => {
-    list_link.classList.remove('active');
-    new_link.classList.remove('active');
-    contact_link.classList.add('active');
-    listSection.classList.add('hidden');
-    contactSection.classList.remove('hidden');
-    newSection.classList.add('hidden');
+  list_link.classList.remove('active');
+  new_link.classList.remove('active');
+  contact_link.classList.add('active');
+  listSection.classList.add('hidden');
+  contactSection.classList.remove('hidden');
+  newSection.classList.add('hidden');
 });
 
 const luxon_time = document.querySelector('#date');
